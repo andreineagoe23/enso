8a522ff208ec23f58285b9897c6573851e5cb4bd
Andrei Neagoe <neagoe.andrei23@yahoo.com>
2025-10-17 21:00:54 +0100

    Build site for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Keep root base for serving from repository root or docs on GitHub Pages
-  base: "/",
+  base: "/enso/",
   build: {
     outDir: "docs",
     assetsDir: "assets",
